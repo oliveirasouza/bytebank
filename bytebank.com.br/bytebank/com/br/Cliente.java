7e160965adefe364752247f3695b73e592870c4d
oliveirasouza00@gmail.com <oliveirasouza00@gmail.com>
2020-02-10 14:27:56 -0300

    gitlab

--- bytebank.com.br/bytebank/com/br/Cliente.java
+++ bytebank.com.br/bytebank/com/br/Cliente.java
@@ -19,13 +19,16 @@ public class Cliente {
 		}
 		
 		
+		
 		public String getNome() {
 			
 			return nome;
 		}
+		
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
+		
 		public String getCpf() {
 			return cpf;
 		}
